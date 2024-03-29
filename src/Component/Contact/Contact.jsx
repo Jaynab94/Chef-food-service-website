9ecf47a4e267705b19c1c4b45bb1c3a64b96a56e
unknown <anisamim94@gmail.com>
2024-03-29 23:06:29 +0600

    add contact section

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -2,8 +2,8 @@
 
 const Contact = () => {
     return (
-        <div>
-            <h2>this is contact</h2>
+        <div  className="text-2xl text-green-700 text-center">
+            <h2>Our team endeavors to respond to all messages promptly. <br /> Thank you for your interest in our books and for visiting our website!</h2>
         </div>
     );
 };
